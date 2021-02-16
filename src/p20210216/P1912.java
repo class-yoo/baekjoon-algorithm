f60873f82a71db4ee91e309c0ff424f8acd39cd6
Yujaehyeong <mynameisyjh@gmail.com>
2021-02-17 01:52:27 +0900

    DP 문제풀이 1912문제 수정

--- src/p20210216/P1912.java
+++ src/p20210216/P1912.java
@@ -5,20 +5,14 @@ import java.util.Scanner;
 public class P1912 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		int n = sc.nextInt();
-
-		int[] arr = new int[n];
-		
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		int max = arr[0];
+		int max = -Integer.MAX_VALUE;
 		int sum = 0;
+		
 		for (int i = 0; i < n; i++) {
-			sum += arr[i];
+			sum += sc.nextInt();
 			max = Math.max(max, sum);
-			
 			if (sum < 0) sum = 0;
 		}
 		
