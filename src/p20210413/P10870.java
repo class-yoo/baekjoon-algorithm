4d8196c39667fa9f0a21ff760693ab25f1610050
Yujaehyeong <mynameisyjh@gmail.com>
2021-04-15 00:16:24 +0900

    백준 1010 다리 놓기 문제 풀이

--- src/p20210413/P10870.java
+++ src/p20210413/P10870.java
@@ -7,11 +7,25 @@ public class P10870 {
 
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        int loop = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            int num1 = sc.nextInt();
-            int num2 = sc.nextInt();
+        int[][] dp = new int [30][30];
+
+        for (int i = 0; i < dp.length ; i++) {
+            dp[0][i] = i+1;
+        }
+
+        for (int i = 1; i < dp.length ; i++) {
+            for (int j = i; j < dp.length ; j++) {
+                dp[i][j] = dp[i-1][j-1] + dp[i][j-1];
+            }
+        }
+
+        for (int i = 0; i < loop; i++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            System.out.println(dp[n-1][m-1]);
         }
     }
 }
