5cc892979e5f9e39f0bca85dcb9bcd0aea58f5ca
Yujaehyeong <mynameisyjh@gmail.com>
2021-04-14 23:54:16 +0900

    백준 10870 피보나치5 문제 풀이

---
+++ src/p20210413/P10870.java
@@ -0,0 +1,17 @@
+package p20210413;
+
+import java.util.Scanner;
+
+public class P10870 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+        }
+    }
+}
