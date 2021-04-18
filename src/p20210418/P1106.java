750cf6b85c74da810c898a141aafb8c7a1f71171
Yujaehyeong <mynameisyjh@gmail.com>
2021-04-18 22:58:59 +0900

    백준 15486 퇴사2 문제풀이

---
+++ src/p20210418/P1106.java
@@ -0,0 +1,40 @@
+package p20210418;
+
+import java.util.Scanner;
+
+public class P1106 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[] dp = new int[c+1];
+
+        for (int i = 1; i <=c; i++) {
+            dp[i] = 100 * 1000;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int price = sc.nextInt();
+            int customerNumber = sc.nextInt();
+            if (dp[customerNumber] != 0) {
+                dp[customerNumber] = Math.min(dp[customerNumber], price);
+            } else {
+                dp[customerNumber] = price;
+            }
+        }
+
+        for (int i = 2; i <= c; i++) {
+            int j = 1;
+            int endPoint = i/2 + 1;
+            while(j < endPoint) {
+                int tempPrice = dp[i-j] + dp[j];
+                dp[i] = dp[i] == 100 * 1000 ? tempPrice : Math.min(tempPrice, dp[i]);
+                j++;
+            }
+        }
+
+        System.out.println(dp[c]);
+    }
+}
