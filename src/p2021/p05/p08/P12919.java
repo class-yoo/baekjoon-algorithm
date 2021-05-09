e41ca68b6c82ad2605084e9e180704c5d4c19597
Yujaehyeong <mynameisyjh@gmail.com>
2021-05-09 22:25:39 +0900

    백준 12919 A와 B 2 문제풀이

---
+++ src/p2021/p05/p08/P12919.java
@@ -0,0 +1,53 @@
+package p2021.p05.p08;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class P12919 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String t = sc.next();
+        int result = 0;
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add(t);
+
+        while(!queue.isEmpty()) {
+
+            String alphabet = queue.poll();
+
+            if (alphabet.charAt(alphabet.length() - 1) == 'A') {
+                String temp = alphabet.substring(0 , alphabet.length() - 1);
+
+                if(temp.length() < s.length()) break;
+
+                if (temp.equals(s)) {
+                    result = 1;
+                    break;
+                }
+
+                queue.add(temp);
+            }
+
+            if (alphabet.charAt(0) == 'B') {
+                StringBuilder sb = new StringBuilder(alphabet.substring(1));
+                String temp = sb.reverse().toString();
+
+                if(temp.length() < s.length()) break;
+
+                if (temp.equals(s)) {
+                    result = 1;
+                    break;
+                }
+
+                queue.add(temp);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
