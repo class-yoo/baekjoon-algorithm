7775ae512bd148e405da0c011f6219e0e427ce7f
Yujaehyeong <mynameisyjh@gmail.com>
2021-05-23 22:52:25 +0900

    백준 20154 이 구역의 승자는 누구야?! 문제풀이

---
+++ src/p2021/p05/p22/P20154.java
@@ -0,0 +1,32 @@
+package p2021.p05.p22;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class P20154 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        ArrayList<Integer> list = new ArrayList<>();
+        int[] table = {3, 2, 1, 2, 3, 3, 3, 3, 1, 1, 3, 1, 3, 3, 1, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2, 1};
+        int[] sumArr = new int[input.length()];
+
+        for (int i = 0; i < input.length(); i++) {
+            sumArr[i] = table[input.charAt(i) - 'A'];
+        }
+
+        int idx = 1;
+
+        while (idx < input.length()) {
+            for (int i = 0; i+idx < input.length(); i++) {
+                int sum = sumArr[i] + sumArr[i + idx];
+                sumArr[i] = sum > 10 ? sum % 10 : sum;
+            }
+            idx *= 2;
+        }
+
+        System.out.println(sumArr[0] % 2 == 1 ? "I'm a winner!" : "You're the winner?");
+    }
+}
