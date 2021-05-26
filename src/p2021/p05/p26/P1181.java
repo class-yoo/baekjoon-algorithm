a2b5c214f9388d18830f181eb5395aaea36f1d97
Yujaehyeong <mynameisyjh@gmail.com>
2021-05-26 23:15:02 +0900

    백준 1181 단어 정렬 문제풀이

--- src/p2021/p05/p26/P1181.java
+++ src/p2021/p05/p26/P1181.java
@@ -26,9 +26,9 @@ public class P1181 {
             public int compare(String o1, String o2) {
                 if (o1.length() == o2.length()) {
                     return o1.compareTo(o2);
-                } else {
-                    return o1.length() > o2.length() ? 1 : -1;
                 }
+
+                return o1.length() > o2.length() ? 1 : -1;
             }
         });
 
