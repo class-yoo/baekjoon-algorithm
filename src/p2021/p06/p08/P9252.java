60007e0f92652c260e6fdabebbdb2941e44bf39b
Yujaehyeong <mynameisyjh@gmail.com>
2021-06-08 23:10:16 +0900

    백준 9252 LCS 2 문제풀이

--- src/p2021/p06/p08/P9252.java
+++ src/p2021/p06/p08/P9252.java
@@ -9,32 +9,44 @@ public class P9252 {
 		Scanner sc = new Scanner(System.in);
 		String word1 = sc.next();
 		String word2 = sc.next();
-		String answerWord = "";
 
 		int countArr[][] = new int[word1.length() + 1][word2.length() + 1];
-		int appendIdx = 0;
-		boolean isNotAppended = true;
 
 		for (int i = 1; i <= word1.length(); i++) {
 			for (int j = 1; j <= word2.length(); j++) {
 				if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
 					countArr[i][j] = countArr[i - 1][j - 1] + 1;
-					if (appendIdx < j && isNotAppended) {
-						appendIdx = j;
-						isNotAppended = false;
-						answerWord += word2.charAt(j - 1);
-						
-					}
 					continue;
 				}
 				countArr[i][j] = Math.max(countArr[i][j - 1], countArr[i - 1][j]);
 			}
-			isNotAppended = true;
 		}
-		
-		System.out.println(countArr[word1.length()][word2.length()]);
-		System.out.println(answerWord);
-		
 
+		int lengthOfLCS = countArr[word1.length()][word2.length()];
+
+		System.out.println(lengthOfLCS);
+
+		int appendIdx = 0;
+		int i = word1.length();
+		int j = word2.length();
+		StringBuilder sb = new StringBuilder();
+
+		while (appendIdx < lengthOfLCS) {
+			if (countArr[i][j] == countArr[i - 1][j]) {
+				i--;
+				continue;
+			}
+			if (countArr[i][j] == countArr[i][j - 1]) {
+				j--;
+				continue;
+			}
+
+			sb.append(word1.charAt(i-1));
+			appendIdx++;
+			i--;
+			j--;
+		}
+
+		System.out.println(sb.reverse().toString());
 	}
 }
