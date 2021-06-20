477ba356fe6173b53eac24011403359e39a25915
Yujaehyeong <mynameisyjh@gmail.com>
2021-06-21 00:58:22 +0900

    백준 21275 폰 호석만 문제풀이

--- src/p2021/p06/p20/P21275.java
+++ src/p2021/p06/p20/P21275.java
@@ -7,28 +7,63 @@ import java.io.InputStreamReader;
 public class P21275 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String A = br.readLine();
-        String B = br.readLine();
-        StringBuilder sb = new StringBuilder();
+        String[] input = br.readLine().split(" ");
+        String A = input[0];
+        String B = input[1];
+        String result = "";
+        int cnt = 0;
 
         for (int i = 2; i <= 36; i++) {
-            boolean isPass = true;
-            int mulNum = 1;
+            boolean isAPass = true;
+            int aMulNum = 1;
             int aTotal = 0;
-            for (int j = A.length() - 1; j >= 0; j++) {
-                if (convertWordToNumber(A.charAt(j)) < i) {
-                    isPass = false;
+            for (int j = A.length() - 1; j >= 0; j--) {
+                int tempNum = convertWordToNumber(A.charAt(j));
+                if (tempNum >= i) {
+                    isAPass = false;
                     break;
                 }
+
+                aTotal += tempNum * aMulNum;
+                aMulNum *= i;
             }
 
-            if (isPass == true) {
+            if (isAPass == true) {
                 for (int k = 2; k <= 36; k++) {
                     if (i != k) {
+                        boolean isBPass = true;
+                        int bMulNum = 1;
+                        int bTotal = 0;
+                        for (int j = B.length() - 1; j >= 0; j--) {
+                            int tempNum = convertWordToNumber(B.charAt(j));
+                            if (tempNum >= k) {
+                                isBPass = false;
+                                break;
+                            }
+
+                            bTotal += tempNum * bMulNum;
+                            bMulNum *= k;
+                        }
+
+                        if(isBPass == true) {
+                            if (aTotal == bTotal) {
+                                cnt++;
+                                result = aTotal + " " + i + " " + k;
+                                break;
+                            }
+                        }
                     }
                 }
             }
         }
+
+        if (cnt > 1) {
+            result = "Multiple";
+        } else if(cnt == 0) {
+            result = "Impossible";
+        }
+
+        System.out.println(result);
     }
 
     public static int convertWordToNumber(char word) {
