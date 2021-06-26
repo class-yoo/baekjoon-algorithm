b913db2c475b6d932df27932bdd978ef46b6c285
Yujaehyeong <mynameisyjh@gmail.com>
2021-06-26 23:14:59 +0900

    백준 1158 요세푸스 문제풀이

---
+++ src/p2021/p06/p26/P1158.java
@@ -0,0 +1,44 @@
+package p2021.p06.p26;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class P1158 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        int[] result = new int [n];
+
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        int removeCheck = 1;
+        int idx = 0;
+
+        while (!queue.isEmpty()) {
+            int number = queue.poll();
+            if (removeCheck++ % k == 0) {
+                result[idx++] = number;
+                continue;
+            }
+            queue.add(number);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<"+result[0]);
+        for (int i = 1; i < n; i++) {
+            sb.append(", ");
+            sb.append(result[i]);
+        }
+        sb.append(">");
+
+        System.out.println(sb.toString());
+    }
+}
