87bdace3033d18aacbde754ffc25379eb00d6edc
Yujaehyeong <mynameisyjh@gmail.com>
2021-07-04 19:35:11 +0900

    백준 2512 예산 문제풀이

---
+++ src/p2021/p07/p04/P2512.java
@@ -0,0 +1,44 @@
+package p2021.p07.p04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P2512 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        int sum = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += array[i] = sc.nextInt();
+        }
+
+        int budget = sc.nextInt();
+        Arrays.sort(array);
+
+        if (sum <= budget) {
+            System.out.println(array[n - 1]);
+        } else {
+            int tmp = budget / n;
+            int remain = n;
+            for (int i = 0; i < n; i++) {
+                if (array[i] <= tmp) {
+                    budget -= array[i];
+                } else {
+                    remain = n - i;
+                    tmp = budget / remain;
+                    if (array[i] > tmp) {
+                        break;
+                    }
+                    i--;
+                }
+            }
+            System.out.println(budget / remain);
+        }
+
+    }
+}
