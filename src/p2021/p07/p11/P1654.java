cbfd6970eb8af28434f2ba75f03b5b5de6877382
Yujaehyeong <mynameisyjh@gmail.com>
2021-07-11 23:27:56 +0900

    백준 이분탐색 재풀이

---
+++ src/p2021/p07/p11/P1654.java
@@ -0,0 +1,47 @@
+package p2021.p07.p11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P1654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int k = Integer.parseInt(input[0]);
+        int n = Integer.parseInt(input[1]);
+
+        int[] cables = new int [k];
+
+        for (int i = 0; i < k; i++) {
+            cables[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(cables);
+
+        long start = 1;
+        long end = cables[k - 1];
+        long mid = 0;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+
+            int cnt = 0;
+            int cableIdx = k - 1;
+
+            while(mid <= cables[cableIdx]) {
+                cnt += cables[cableIdx--] / mid;
+                if (cableIdx < 0) break;
+            }
+
+            if (cnt >= n ) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        System.out.println(end);
+    }
+}
---
+++ src/p2021/p07/p11/P2805.java
@@ -0,0 +1,44 @@
+package p2021.p07.p11;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P2805 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int start = 0;
+        int end = arr[n-1];
+        int mid = 0;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+
+            int idx = arr.length - 1;
+            long sum = 0;
+            while(arr[idx] > mid) {
+                sum += arr[idx--] - mid ;
+                if (idx < 0) break;
+            }
+
+            if (sum >= m) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        System.out.println(end);
+    }
+}
