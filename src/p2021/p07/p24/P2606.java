adb996d6925a22527e63a5dfc70de895a30d8345
Yujaehyeong <mynameisyjh@gmail.com>
2021-07-25 02:57:30 +0900

    백준 2606 바이러스 문제풀이

--- src/p2021/p07/p24/P2606.java
+++ src/p2021/p07/p24/P2606.java
@@ -1,46 +1,45 @@
 package p2021.p07.p24;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Scanner;
 
 public class P2606 {
+    static boolean[][] network;
+    static boolean[] isInfected;
+    static int infectCnt;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int com = sc.nextInt();
         int connectCnt = sc.nextInt();
-        Deque<Integer> infectedQueue = new ArrayDeque<>();
 
-        boolean [][] network = new boolean[com + 1][com + 1];
-        boolean[] isInfected = new boolean[com + 1];
-        int infectCnt = 0;
+        network = new boolean[com + 1][com + 1];
+        isInfected = new boolean[com + 1];
+        infectCnt = 0;
 
         for (int i = 0; i < connectCnt; i++) {
             int dep = sc.nextInt();
             int dest = sc.nextInt();
 
-            network[dep][dest] = true;
-
-            if (dep == 1) {
-                infectedQueue.add(dest);
-                isInfected[dest] = true;
-                infectCnt++;
-            }
+            network[dep][dest] = network[dest][dep] = true;
         }
 
-        while (!infectedQueue.isEmpty()) {
-            int dep = infectedQueue.poll();
-            for (int i = 2; i <= com; i++) {
-                if (network[dep][i]) {
-                    if (!isInfected[i]) {
-                        isInfected[i] = true;
-                        infectedQueue.add(i);
-                        infectCnt++;
-                    }
+        dfs(1);
+
+        System.out.println(infectCnt);
+    }
+
+
+    public static void dfs(int dep) {
+        isInfected[dep] = true;
+
+        for (int i = 1; i < network.length; i++) {
+            if (network[dep][i] == true) {
+                if (isInfected[i] == false) {
+                    infectCnt++;
+                    dfs(i);
                 }
             }
         }
 
-        System.out.println(infectCnt);
     }
 }
