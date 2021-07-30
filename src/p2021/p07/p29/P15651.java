a47c91454c40847db7332c0fc487f0add7a6eb3f
Yujaehyeong <mynameisyjh@gmail.com>
2021-07-30 23:25:52 +0900

    백준 15651 N과 M (3) 문제풀이

--- src/p2021/p07/p29/P15651.java
+++ src/p2021/p07/p29/P15651.java
@@ -6,24 +6,25 @@ public class P15651 {
     static int[] numbers;
     static int n;
     static int m;
+    static StringBuilder sb;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        sb = new StringBuilder();
         n = sc.nextInt();
         m = sc.nextInt();
         numbers = new int[m];
 
         dfs(0);
+        System.out.println(sb.toString());
     }
-
 
     public static void dfs(int idx) {
         if (idx == m) {
             for (int i = 0; i < m; i++) {
-                System.out.print(numbers[i] + " ");
+                sb.append(numbers[i] + " ");
             }
-            System.out.println();
-
+            sb.append("\n");
             return;
         }
 
