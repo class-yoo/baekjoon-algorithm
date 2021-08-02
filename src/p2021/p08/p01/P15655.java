fba75cd71faa044ad877a99270006a9f0ef7af20
Yujaehyeong <mynameisyjh@gmail.com>
2021-08-02 23:30:23 +0900

    백준 15655 N과 M (6) 문제풀이

---
+++ src/p2021/p08/p01/P15655.java
@@ -0,0 +1,44 @@
+package p2021.p08.p01;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P15655 {
+    static int[] numbers;
+    static int[] initNumbers;
+    static int n;
+    static int m;
+    static StringBuilder sb;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sb = new StringBuilder();
+        n = sc.nextInt();
+        m = sc.nextInt();
+        initNumbers = new int[n];
+        numbers = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            initNumbers[i] = sc.nextInt();
+        }
+
+        Arrays.sort(initNumbers);
+        dfs(0, 0);
+        System.out.println(sb.toString());
+    }
+
+    public static void dfs(int idx, int preIdx) {
+        if (idx == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(numbers[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = preIdx; i < n; i++) {
+            numbers[idx] = initNumbers[i];
+            dfs(idx + 1, i + 1);
+        }
+    }
+}
