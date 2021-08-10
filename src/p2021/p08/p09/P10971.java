852d0d2d6a6a20e58ccb3c3ce3736b902ade1da3
Yujaehyeong <mynameisyjh@gmail.com>
2021-08-10 21:43:57 +0900

    백준 10971 외판원 순회 2 문제풀이

---
+++ src/p2021/p08/p09/P10971.java
@@ -0,0 +1,51 @@
+package p2021.p08.p09;
+
+import java.util.Scanner;
+
+public class P10971 {
+    static boolean[] isVisited;
+    static int [][] map;
+    static int n;
+    static int dest;
+    static int min;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        isVisited = new boolean[n];
+        map = new int[n][n];
+        min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            dest = i;
+            dfs(0, i, 0);
+        }
+
+        System.out.println(min);
+    }
+
+
+    public static void dfs(int visitCnt, int curCountry, int price) {
+        if (visitCnt == n && curCountry == dest) {
+            min = Math.min(min, price);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!isVisited[i]) {
+                if (map[curCountry][i] > 0) {
+                    isVisited[i] = true;
+                    dfs(visitCnt + 1, i, price + map[curCountry][i]);
+                    isVisited[i] = false;
+                }
+            }
+        }
+    }
+}
