854cb3a97ebea0d23f9dc6cd9036829370f441a3
Yujaehyeong <mynameisyjh@gmail.com>
2021-08-16 18:04:04 +0900

    백준 16987 계란으로 계란치기 문제풀이

--- src/p2021/p08/p15/P16987.java
+++ src/p2021/p08/p15/P16987.java
@@ -15,16 +15,18 @@ public class P16987 {
         eggs = new int [n][2];
         isBroken = new boolean[n];
 
-
         for (int i = 0; i < n; i++) {
             eggs[i][0] = sc.nextInt();
             eggs[i][1] = sc.nextInt();
         }
+
+        dfs(0,0);
+        System.out.println(max);
     }
 
 
     public static void dfs(int cnt, int idx) {
-        if (idx == n) {
+        if (idx >= n || cnt >= n-1) {
             max = Math.max(max, cnt);
             return;
         }
@@ -32,9 +34,9 @@ public class P16987 {
         for (int i = 0; i < n; i++) {
             if (i == idx) continue;
             if (!isBroken[i]) {
-                int next = 0;
+                int originCnt = cnt;
                 int temp1 = eggs[i][0];
-                eggs[i][0] = eggs[idx][1] - eggs[i][0];
+                eggs[i][0] = eggs[i][0] - eggs[idx][1];
 
                 if (eggs[i][0] <= 0) {
                     isBroken[i] = true;
@@ -42,12 +44,24 @@ public class P16987 {
                 }
 
                 int temp2 = eggs[idx][0];
-                eggs[idx][0] = eggs[i][1] - eggs[idx][0];
-
+                eggs[idx][0] = eggs[idx][0] - eggs[i][1];
                 if (eggs[idx][0] <= 0) {
                     isBroken[idx] = true;
                     cnt++;
                 }
+
+                int copiedIdx = idx;
+                copiedIdx++;
+
+                while (copiedIdx < n && isBroken[copiedIdx]) {
+                    copiedIdx++;
+                }
+
+                dfs(cnt, copiedIdx);
+                eggs[i][0] = temp1;
+                eggs[idx][0] = temp2;
+                isBroken[idx] = isBroken[i] = false;
+                cnt = originCnt;
             }
 
         }
